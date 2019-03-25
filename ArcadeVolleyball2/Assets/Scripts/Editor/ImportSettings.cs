30811149a5a1bc4c62265251f246287a19067318
Benjamin <benhasanemail@gmail.com>
2019-03-25 14:27:01 -0400

    added .gitignore and unity project

---
+++ ArcadeVolleyball2/Assets/Scripts/Editor/ImportSettings.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ImportSettings : AssetPostprocessor
+{
+    /*
+     *beshken was here XD love u ben
+     */
+
+    bool postProcess;
+    int ppu = 32;
+
+    private void OnPreprocessTexture()
+    {
+        Debug.Log("importing: "+assetPath);
+
+        TextureImporter ti = (TextureImporter) assetImporter;
+        ti.textureType = TextureImporterType.Sprite;
+        ti.spritePixelsPerUnit = ppu;
+        ti.mipmapEnabled = false;
+        ti.filterMode = FilterMode.Point;
+
+        if (assetPath.Contains("guy"))
+        {
+            ti.spriteImportMode = SpriteImportMode.Multiple;
+            postProcess = true;
+        }
+        else
+        {
+            ti.spriteImportMode = SpriteImportMode.Single;
+            postProcess = false;
+        }
+    }
+
+    private void OnPostprocessTexture(Texture2D texture)
+    {
+        if (!postProcess) return;
+        
+        string fileName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
+        int spriteSize = texture.height;
+        
+        var metas = new List<SpriteMetaData>();
+
+
+        for (int c = 0; c < texture.width/texture.height; c++)
+        {
+            SpriteMetaData meta = new SpriteMetaData();
+            meta.rect = new Rect(c * spriteSize, 0, spriteSize, spriteSize);
+            meta.name = fileName + "_" + c;
+            metas.Add(meta);
+        }
+        
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        textureImporter.spritePixelsPerUnit = spriteSize / 2;
+        textureImporter.spritesheet = metas.ToArray();
+    }
+}
---
+++ ArcadeVolleyball2/Assets/Scripts/Skins/BackgroundSet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Background")]
+public class BackgroundSet : ScriptableObject
+{
+    [Header("General")]
+    public string Name;
+	
+    [Header("Background")]
+    public Sprite BackgroundSprite;
+    public Color BackgroundColor = Color.black;
+
+    [Header("Ball")] 
+    public Sprite BallSprite;
+    public Color BallColor = Color.white;
+
+    [Header("GUI")] 
+    public Color TitleColor = Color.blue;
+    public Color ScoreColor = Color.white;
+    public Color UiColor = Color.red;
+}
